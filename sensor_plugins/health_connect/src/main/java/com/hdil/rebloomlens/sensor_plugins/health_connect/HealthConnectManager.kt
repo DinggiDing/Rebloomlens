c42c5294c6b180a0f8269af44e8da926c0c4d236
hdilys <hdil.yonsei@gmail.com>
2025-04-11 16:36:05 +0900

    04.11 16:36

--- sensor_plugins/health_connect/src/main/java/com/hdil/rebloomlens/sensor_plugins/health_connect/HealthConnectManager.kt
+++ sensor_plugins/health_connect/src/main/java/com/hdil/rebloomlens/sensor_plugins/health_connect/HealthConnectManager.kt
@@ -20,7 +20,14 @@ import androidx.health.connect.client.records.HeartRateRecord
 import androidx.health.connect.client.records.SleepSessionRecord
 import androidx.health.connect.client.records.StepsRecord
 import androidx.health.connect.client.records.WeightRecord
+import androidx.health.connect.client.request.AggregateRequest
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import com.hdil.rebloomlens.sensor_plugins.health_connect.sleep.SleepSessionData
 import org.json.JSONArray
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+import kotlin.time.Instant
 
 
 // Utilized by https://github.com/android/health-samples
@@ -103,5 +110,43 @@ class HealthConnectManager(
 
     suspend fun revokeAllPermissions() {
         healthConnectClient.permissionController.revokeAllPermissions()
+    }
+
+
+    suspend fun readSleepSessions(): List<SleepSessionData> {
+        val lastDay = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS)
+            .minusDays(1)
+            .withHour(12)
+        val firstDay = lastDay.minusDays(7)
+
+        val sessions = mutableListOf<SleepSessionData>()
+        val sleepSessionRequest = ReadRecordsRequest(
+            recordType = SleepSessionRecord::class,
+            timeRangeFilter = TimeRangeFilter.between(firstDay.toInstant(), lastDay.toInstant()),
+            ascendingOrder = false
+        )
+        val sleepSessions = healthConnectClient.readRecords(sleepSessionRequest)
+        sleepSessions.records.forEach { session ->
+            val sessionTimeFilter = TimeRangeFilter.between(session.startTime, session.endTime)
+            val durationAggregateRequest = AggregateRequest(
+                metrics = setOf(SleepSessionRecord.SLEEP_DURATION_TOTAL),
+                timeRangeFilter = sessionTimeFilter
+            )
+            val aggregateResponse = healthConnectClient.aggregate(durationAggregateRequest)
+            sessions.add(
+                SleepSessionData(
+                    uid = session.metadata.id,
+                    title = session.title,
+                    notes = session.notes,
+                    startTime = session.startTime,
+                    startZoneOffset = session.startZoneOffset,
+                    endTime = session.endTime,
+                    endZoneOffset = session.endZoneOffset,
+                    duration = aggregateResponse[SleepSessionRecord.SLEEP_DURATION_TOTAL],
+                    stages = session.stages
+                )
+            )
+        }
+        return sessions
     }
 }
